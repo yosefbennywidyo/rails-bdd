3d6f5d5258935f66c580fd41edc7dc9e82c2cabc
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-09-27 04:17:06 +0000

    Fixed root

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get 'post/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "posts#index"
+  root to: "post#index"
 end
