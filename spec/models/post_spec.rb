9a2ba4a5a4612d7fc043258b7a01b4046e8e42e0
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-09-27 03:50:16 +0000

    Added post test with Rspec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of(:title) }
+  it { is_expected.to validate_length_of(:title).is_at_least(5) }
+  it { is_expected.to validate_presence_of(:content) }
+  it { is_expected.to validate_length_of(:content).is_at_least(10) }
 end
